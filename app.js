15ff7647e591217140854e5affa063d8b0f52477
Jose Antonio <jose@jose-HP>
2024-05-22 14:01:58 +0200

    Proyecto con Bun

---
+++ app.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import Event from './models/Event.js'; // Asegúrate de que la ruta al archivo es correcta
+
+const app = express();
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
+
+mongoose.connect('mongodb://localhost:27017/humanityEvents')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
+
+app.post('/events', async (req, res) => {
+  const event = new Event(req.body);
+  try {
+    await event.save();
+    res.status(201).send(event);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/events', async (req, res) => {
+  try {
+    const events = await Event.find({});
+    res.send(events);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/events/:id', async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) {
+      res.status(404).send({ message: "Evento no encontrado" });
+    } else {
+      res.send(event);
+    }
+  } catch (error) {
+    console.error("Error al recuperar el evento:", error);
+    res.status(500).send({ message: "Error al recuperar el evento" });
+  }
+});
+
+app.patch('/events/:id', async (req, res) => {
+  try {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!event) {
+      return res.status(404).send();
+    }
+    res.send(event);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete('/events/:id', async (req, res) => {
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id);
+    if (!event) {
+      return res.status(404).send();
+    }
+    res.send(event);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000/events');
+});
---
+++ models/Event.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+export default Event;
